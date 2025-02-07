f087a05deaf857eb859358bdd8d76d4ea17d8ee8
GRL <djxreddmgmt@gmail.com>
2025-02-07 03:58:30 -0500

    testing
    testing

---
+++ artist_manager_agent/__init__.py
@@ -0,0 +1,1 @@
+"""Artist Manager Agent package.""" 
--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,9 +1,6 @@
 def test_imports():
     """Test that we can import the required modules."""
-    from artist_manager_agent.team_management import PaymentMethod, PaymentStatus, PaymentRequest
-    from artist_manager_agent.main import ArtistManagerBot
+    from artist_manager_agent.team_management import PaymentMethod, TeamManager
     
     assert PaymentMethod is not None
-    assert PaymentStatus is not None
-    assert PaymentRequest is not None
-    assert ArtistManagerBot is not None 
+    assert TeamManager is not None 
